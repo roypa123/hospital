4e8ecdc9d72b3f392442cdd6eb8989c986f0055e
Roy-P-A <roy.pa@nintriva.io>
2026-07-07 16:22:56 +0530

    fix: some changes

--- hospital-backend/app.js
+++ hospital-backend/app.js
@@ -1,11 +1,22 @@
 const express = require("express");
+const cors = require("cors");
+const helmet = require("helmet");
+const compression = require("compression");
+const morgan = require("morgan");
 
 const app = express();
 
+app.use(cors());
+app.use(helmet());
+app.use(compression());
+app.use(morgan("dev"));
 app.use(express.json());
 
 app.get("/", (req, res) => {
-    res.send("Hospital API Running");
+    res.json({
+        success: true,
+        message: "Hospital API Running"
+    });
 })
 
 module.exports = app;
