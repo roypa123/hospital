6068ad9e123e63745685e6d9358360bb87c1f297
Roy-P-A <roy.pa@nintriva.io>
2026-07-13 12:20:52 +0530

    fix: some changes

--- hospital-frontend/src/App.tsx
+++ hospital-frontend/src/App.tsx
@@ -1,15 +1,13 @@
 
 import './App.css'
+import { Button } from "@/components/ui/button";
 
 function App() {
   
 
   return (
-    <div>
-     <h1 className="text-3xl font-bold underline">
-    Hello world!
-  </h1>
-     
+   <div className="flex items-center justify-center h-screen">
+      <Button>Click Me</Button>
     </div>
   )
 }
