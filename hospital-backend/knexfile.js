95b9c10e45450fa735c51dcd95b4f3a76a54d9de
roypa123 <roypa81130@gmail.com>
2026-09-05 16:09:16 +0530

    fix: some changes

--- hospital-backend/knexfile.js
+++ hospital-backend/knexfile.js
@@ -1,33 +1,38 @@
 require("dotenv").config();
 
+const config = {
+
+    client: "pg",
+
+    connection: {
+
+        host: process.env.DB_HOST,
+
+        port: process.env.DB_PORT,
+
+        user: process.env.DB_USER,
+
+        password: process.env.DB_PASSWORD,
+
+        database: process.env.DB_NAME
+    },
+
+    migrations: {
+
+        directory: "./src/database/migrations"
+    },
+
+    seeds: {
+
+        directory: "./src/database/seeds"
+    }
+
+};
+
 module.exports = {
 
-    development: {
+    development: config,
 
-        client: "pg",
-
-        connection: {
-
-            host: process.env.DB_HOST,
-
-            port: process.env.DB_PORT,
-
-            user: process.env.DB_USER,
-
-            password: process.env.DB_PASSWORD,
-
-            database: process.env.DB_NAME
-        },
-
-        migrations: {
-
-            directory: "./src/database/migrations"
-        },
-
-        seeds: {
-
-            directory: "./src/database/seeds"
-        }
-    }
+    production: config
 
 };
